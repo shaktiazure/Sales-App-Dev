bd0c4e80abc2785813850ecd0f58751bd01137ac
Laxman Chavan <laxman.chavan05@yahoo.com>
2024-04-27 17:34:08 +0530

    resources deployed

--- Terraform-Dev/RG/Devrg.tf
+++ Terraform-Dev/RG/Devrg.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.101.0"
     }
   }
@@ -9,4 +9,14 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
+  features {
+
+  }
+
 }
+
+resource "azurerm_resource_group" "devRG" {
+  name     = "Sales-Dev-RG"
+  location = "West Europe"
+}
+
