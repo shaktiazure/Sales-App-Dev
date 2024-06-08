c34119f309831bf7c96b373367a77bbe3a6f8f61
Laxman Chavan <laxman.chavan05@yahoo.com>
2024-06-08 15:52:18 +0530

    changes

--- Terraform-Dev/RG/Devrg.tf
+++ Terraform-Dev/RG/Devrg.tf
@@ -5,6 +5,14 @@ terraform {
       version = "3.101.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "devRG"
+    storage_account_name = "sdevstorage"
+    container_name       = "sdevContainer"
+    # key                  = "terraform.tfstate"
+  }
+
 }
 
 provider "azurerm" {
@@ -14,6 +22,8 @@ provider "azurerm" {
   }
 
 }
+
+
 
 resource "azurerm_resource_group" "devRG" {
   name     = "Sales-Dev-RG"
